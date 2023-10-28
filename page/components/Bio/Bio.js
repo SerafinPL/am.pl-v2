4af1b7e678e5a66011f2bd05d2d2c6df6115a61f
serafinpl <ser.burberibar@gmail.com>
2023-10-28 13:19:01 -0200

    links add and change bio content

--- page/components/Bio/Bio.js
+++ page/components/Bio/Bio.js
@@ -7,27 +7,30 @@ import blurStyle from "../../src/styles/blurStyle.module.scss";
 const Bio = (props) => {
   return (
     <div className={`${styles.Bio} ${props.blured && blurStyle.blured}`} id='bio'>
-      <h1>Bio</h1>
+      <h1>Bio</h1><br/>
 
       <p>
         Absolwentka malarstwa Akademii Sztuk Pięknych w Katowicach.
         Dyplom realizowała w pracowni malarstwa prof.
         Andrzeja Tobisa w 2021 roku.
-        Jej sztuka jest niezwykle emocjonalna i osobista, oparta na wątkach autobiograficznych, 
+        Jej sztuka jest niezwykle emocjonalna i osobista, oparta na wątkach autobiograficznych,
         jednak poprzez bogactwo symboli oraz nawiązań do klasycznego malarstwa, dotyka tematów uniwersalnych.
-        Jej oniryczne, ekspresyjne obrazy eksplorują obszary podświadomości, 
-        inspiracje często znajduje we wspomnieniach z dzieciństwa i snach, korzystając między innymi z techniki świadomego śnienia. 
+        Jej oniryczne, ekspresyjne obrazy eksplorują obszary podświadomości,
+        inspiracje często znajduje we wspomnieniach z dzieciństwa i snach, korzystając między innymi z techniki świadomego śnienia.
         Interesują ją zagadnienia związane z psychologią i duchowością.
       </p>
 
-      <h1>Wybrane wystawy:</h1>
+      <h1>Wybrane wystawy</h1><br/>
 
       <ul>
-        <li>Goddamn hippies, wystawa indywidualna, Galeria Pojedyncza, 2017
+        <li>
+          Goddamn hippies, wystawa indywidualna, Galeria Pojedyncza, 2 - 15.03.2017
         </li>
-        <li>Przebudzenia, wystawa zbiorowa, BWA Katowice, 2021
+        <li><a target="_blank" href="http://bwa.katowice.pl/wystawy-i-wydarzenia/przebudzenia-wystawa-podyplomowa-wydzialu-artystycznego-akademii-sztuk-pieknych-w-katowicach/">
+          Przebudzenia, wystawa zbiorowa</a>, BWA Katowice, 2021
         </li>
-        <li>Spacer w nieznane, wystawa zbiorowa, Galeria GX, 2023
+        <li><a target="_blank" href="https://gxgaleriasztuki.pl/spacer-w-nieznane-showroom/">
+          Spacer w nieznane, wystawa zbiorowa</a>, Galeria GX, 2023
         </li>
       </ul>
 
