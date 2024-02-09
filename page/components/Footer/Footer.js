2cd3d54e28783f03ad3baacb7203975f01a7ed4f
serafinpl <ser.burberibar@gmail.com>
2024-02-09 08:34:55 -0200

    better polish footer

--- page/components/Footer/Footer.js
+++ page/components/Footer/Footer.js
@@ -4,12 +4,16 @@ import styles from "./Footer.module.scss";
 import blurStyle from "../../src/styles/blurStyle.module.scss";
 
 
-const Footer = ({blured}) => {
+const Footer = ({ blured }) => {
+
+  const d = new Date();
+  let year = d.getFullYear();
+
   return (
     <div className={`${styles.firstBox} ${blured && blurStyle.blured}`} >
       <div className={styles.secondBox}>
         <h3>
-          Designed by <a href="anitamachura.pl">Anita Machura</a> powered with ❤️ by <a href="https://kubakoder.pl"> Kuba Koder with &#169;</a>
+          Projekt <a href="anitamachura.pl">Anita Machura</a> wykonanie z ❤️ <a href="https://kubakoder.pl">Kuba Koder</a> &#169; 2023 - {year}
         </h3>
       </div>
     </div>
