baff879965d415ed7716d1cb743879e6bcc3595d
serafinpl <ser.burberibar@gmail.com>
2024-03-21 12:09:56 -0200

    nano + version

--- page/components/Footer/Footer.js
+++ page/components/Footer/Footer.js
@@ -18,7 +18,7 @@ const Footer = ({ blured }) => {
         <h3>
         {langAnswer(isLang,"Projekt ", 'Design ')}
            <a href="https://anitamachura.pl">Anita Machura</a> 
-           {langAnswer(isLang," wykonanie z ❤️ ", ' made with ❤️ ')}<a href={langAnswer(isLang,"https://kubakoder.pl",'https://jackthecoder.dev/')}>{langAnswer(isLang,"Kuba Koder", 'Jack the Coder')}</a> &#169; v2.1.13 2023 - {year}
+           {langAnswer(isLang," wykonanie z ❤️ ", ' made with ❤️ ')}<a href={langAnswer(isLang,"https://kubakoder.pl",'https://jackthecoder.dev/')}>{langAnswer(isLang,"Kuba Koder", 'Jack the Coder')}</a> &#169; v2.1.15 2023 - {year}
           
         </h3>
       </div>
